332797a482752441caccec050ab90a68a27e4b02
domparsons <puffins_fridge0d@icloud.com>
2023-11-12 10:51:17 +0000

    Initial Commit

---
+++ Shoey/ShoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeyApp.swift
+//  Shoey
+//
+//  Created by Dom Parsons on 12/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
